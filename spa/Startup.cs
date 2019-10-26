e898ee292b6dcd579a05777441d80013969a2893
castriganoj <castriganoj@gmail.com>
2019-10-26 16:54:34 -0400

    Remove unused references in startup.

--- spa/Startup.cs
+++ spa/Startup.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.FileProviders;
 using spa.Utilities.Middleware;
 
 namespace spa {
